16b353619328bb3e99214de377ffb99b0151fd00
Leroy <leroyp2806@gmail.com>
2019-05-28 08:01:45 +0200

    Fixed inline comment errors

--- 9_OOP_Classes_Instances.py
+++ 9_OOP_Classes_Instances.py
@@ -10,6 +10,8 @@ Instance vars contain data that is UNIQUE to each instance.
 
 # Class is a blueprint for creating instance
 class Employee:
+    """Class containing Employees"""
+
     # Initialise // Constructor method (self is the instance. e.g. emp_1.first = "Meh")
     def __init__(self, first, last, pay):
         # Instance Variables Below
@@ -24,36 +26,33 @@ class Employee:
         return f"{self.first} {self.last}"
 
 
-"""
 # emp_1 and emp_2 are there own unique instances of the class employee
-emp_1 = Employee()
-emp_2 = Employee()
+# emp_1 = Employee()
+# emp_2 = Employee()
 
 # Instance variables
-Don't get much benefit from classes if done as per below.
+# Don't get much benefit from classes if done as per below.
 
-emp_1.first = "Leroy"
-emp_1.last = "Petersen"
-emp_1.email = f"{emp_1.first}.{emp_1.last}@company.com"
-emp_1.pay = 50000
+# emp_1.first = "Leroy"
+# emp_1.last = "Petersen"
+# emp_1.email = f"{emp_1.first}.{emp_1.last}@company.com"
+# emp_1.pay = 50000
 
-emp_2.first = "Test"
-emp_2.last = "User"
-emp_2.email = f"{emp_1.first}.{emp_1.last}@company.com"
-emp_2.pay = 50000
-"""
+# emp_2.first = "Test"
+# emp_2.last = "User"
+# emp_2.email = f"{emp_1.first}.{emp_1.last}@company.com"
+# emp_2.pay = 50000
 
 
-emp_1 = Employee("Leroy", "Petersen", 50000)
-emp_2 = Employee("Test", "User", 60000)
+EMP_1 = Employee("Leroy", "Petersen", 50000)
+EMP_2 = Employee("Test", "User", 60000)
 
 # print(emp_1)
 # print(emp_2)
 
-print(emp_1.email)
-print(emp_2.email)
+print(EMP_1.email)
+print(EMP_2.email)
 
 # Similar, but not exactly the same
-print(Employee.fullname(emp_1))
-print(emp_1.fullname())
-
+print(Employee.fullname(EMP_1))
+print(EMP_1.fullname())
